b48b74b4c9998579d2b2380c0b791b9a797d7ad8
rodney31 <67178645+rodney31@users.noreply.github.com>
2024-03-31 17:53:59 -0400

    Update BinaryControllerTest.java

--- BinaryCalculatorWebapp/src/test/java/com.ontariotechu.sofe3980U/BinaryControllerTest.java
+++ BinaryCalculatorWebapp/src/test/java/com.ontariotechu.sofe3980U/BinaryControllerTest.java
@@ -48,6 +48,14 @@ public class BinaryControllerTest {
 			.andExpect(model().attribute("operand1", "111"))
 			.andExpect(model().attribute("operand1Focused", true));
     }
+    @Test
+    public void getParameter2() throws Exception {
+        this.mvc.perform(get("/").param("operand1","0"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("calculator"))
+                .andExpect(model().attribute("operand1", "0"))
+                .andExpect(model().attribute("operand1Focused", true));
+    }
 	@Test
 	    public void postParameter() throws Exception {
         this.mvc.perform(post("/").param("operand1","111").param("operator","+").param("operand2","111"))//.andDo(print())
@@ -57,4 +65,199 @@ public class BinaryControllerTest {
 			.andExpect(model().attribute("operand1", "111"));
     }
 
+    @Test
+    public void postParameter2() throws Exception {
+        this.mvc.perform(post("/").param("operand1","0").param("operator","+").param("operand2","0"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("result"))
+                .andExpect(model().attribute("result", "0"))
+                .andExpect(model().attribute("operand1", "0"));
+    }
+
+    @Test
+    public void postParameter3() throws Exception {
+        this.mvc.perform(post("/").param("operand1","100").param("operator","+").param("operand2","110"))
+                .andExpect(status().isOk())
+                .andExpect(model().attribute("operand2", "110"));
+    }
+
+    /**
+     * Test The multiply function with values of the same length
+     */
+    @Test
+    public void multiply() throws Exception {
+        this.mvc.perform(post("/").param("operand1","1100100").param("operator","*").param("operand2","1110101"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("result"))
+                .andExpect(model().attribute("result", "10110110110100"))
+                .andExpect(model().attribute("operand1", "1100100"));
+    }
+
+    /**
+     * Test The multiply function with the first value having a length less than the second value
+     */
+    @Test
+    public void multiply2() throws Exception {
+        this.mvc.perform(post("/").param("operand1","1010").param("operator","*").param("operand2","110100"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("result"))
+                .andExpect(model().attribute("result", "1000001000"))
+                .andExpect(model().attribute("operand1", "1010"));
+    }
+
+    /**
+     * Test The multiply function with the first value having a length greater than the second value
+     */
+    @Test
+    public void multiply3() throws Exception {
+        this.mvc.perform(post("/").param("operand1","101010").param("operator","*").param("operand2","110"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("result"))
+                .andExpect(model().attribute("result", "11111100"))
+                .andExpect(model().attribute("operand1", "101010"));
+    }
+
+    /**
+     * Test The multiply function with one value as a zero
+     */
+    @Test
+    public void multiply4() throws Exception {
+        this.mvc.perform(post("/").param("operand1","0").param("operator","*").param("operand2","1110"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("result"))
+                .andExpect(model().attribute("result", "0"))
+                .andExpect(model().attribute("operand1", "0"));
+    }
+
+    /**
+     * Test The multiply function with both values as a zero
+     */
+    @Test
+    public void multiply5() throws Exception {
+        this.mvc.perform(post("/").param("operand1","0").param("operator","*").param("operand2","0"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("result"))
+                .andExpect(model().attribute("result", "0"))
+                .andExpect(model().attribute("operand1", "0"));
+    }
+
+    /**
+     * Test The bitwiseOR function with values of the same length
+     */
+    @Test
+    public void bitwiseOR() throws Exception {
+        this.mvc.perform(post("/").param("operand1","1110").param("operator","|").param("operand2","1100"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("result"))
+                .andExpect(model().attribute("result", "1110"))
+                .andExpect(model().attribute("operand1", "1110"));
+    }
+
+    /**
+     * Test The bitwiseOR function with the first value having a length less than the second value
+     */
+    @Test
+    public void bitwiseOR2() throws Exception {
+        this.mvc.perform(post("/").param("operand1","101").param("operator","|").param("operand2","1000"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("result"))
+                .andExpect(model().attribute("result", "1101"))
+                .andExpect(model().attribute("operand1", "101"));
+    }
+
+    /**
+     * Test The bitwiseOR function with the first value having a length greater than the second value
+     */
+    @Test
+    public void bitwiseOR3() throws Exception {
+        this.mvc.perform(post("/").param("operand1","111100").param("operator","|").param("operand2","100"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("result"))
+                .andExpect(model().attribute("result", "111100"))
+                .andExpect(model().attribute("operand1", "111100"));
+    }
+
+    /**
+     * Test The bitwiseOR function with one value as a zero
+     */
+    @Test
+    public void bitwiseOR4() throws Exception {
+        this.mvc.perform(post("/").param("operand1","0").param("operator","|").param("operand2","1101"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("result"))
+                .andExpect(model().attribute("result", "1101"))
+                .andExpect(model().attribute("operand1", "0"));
+    }
+
+    /**
+     * Test The bitwiseOR function with both values as a zero
+     */
+    @Test
+    public void bitwiseOR5() throws Exception {
+        this.mvc.perform(post("/").param("operand1","0").param("operator","|").param("operand2","0"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("result"))
+                .andExpect(model().attribute("result", "0"))
+                .andExpect(model().attribute("operand1", "0"));
+    }
+
+    /**
+     * Test The bitwiseAND function with values of the same length
+     */
+    @Test
+    public void bitwiseAND() throws Exception {
+        this.mvc.perform(post("/").param("operand1","111").param("operator","&").param("operand2","101"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("result"))
+                .andExpect(model().attribute("result", "101"))
+                .andExpect(model().attribute("operand1", "111"));
+    }
+
+    /**
+     * Test The bitwiseAND function with the first value having a length less than the second value
+     */
+    @Test
+    public void bitwiseAND2() throws Exception {
+        this.mvc.perform(post("/").param("operand1","1000").param("operator","&").param("operand2","101011"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("result"))
+                .andExpect(model().attribute("result", "1000"))
+                .andExpect(model().attribute("operand1", "1000"));
+    }
+
+    /**
+     * Test The bitwiseAND function with the first value having a length greater than the second value
+     */
+    @Test
+    public void bitwiseAND3() throws Exception {
+        this.mvc.perform(post("/").param("operand1","1011101").param("operator","&").param("operand2","1101"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("result"))
+                .andExpect(model().attribute("result", "1101"))
+                .andExpect(model().attribute("operand1", "1011101"));
+    }
+
+    /**
+     * Test The bitwiseAND function with one value as a zero
+     */
+    @Test
+    public void bitwiseAND4() throws Exception {
+        this.mvc.perform(post("/").param("operand1","0").param("operator","&").param("operand2","110110"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("result"))
+                .andExpect(model().attribute("result", "0"))
+                .andExpect(model().attribute("operand1", "0"));
+    }
+
+    /**
+     * Test The bitwiseAND function with both values as a zero
+     */
+    @Test
+    public void bitwiseAND5() throws Exception {
+        this.mvc.perform(post("/").param("operand1","0").param("operator","&").param("operand2","0"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("result"))
+                .andExpect(model().attribute("result", "0"))
+                .andExpect(model().attribute("operand1", "0"));
+    }
 }
